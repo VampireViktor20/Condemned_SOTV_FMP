7d5ea84161bf352d9fa17df448d31fb3c01b5a75
VampireViktor20 <kylecornw3ll@outlook.com>
2022-11-25 14:22:47 +0000

    Examine System Attempt 1
    Simple Examine Mechanics Inputted.
    Text and alignment to be applied.

---
+++ Condemned_SOTV_FMP/Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Examine examine;
+    public PlayerMovement player;
+
+ 
+
+    public IEnumerator ItemAdded()
+    {
+        examine.GetComponent<Examine>().onExamine = false;
+        player.enabled = true;
+        gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
+        Destroy(gameObject);
+    }
+}
